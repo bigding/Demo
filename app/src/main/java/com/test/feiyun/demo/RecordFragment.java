84e0eb4f31659eae5325a2b5e52c80be6bfe61d9
bigding <1037196370@qq.com>
2016-08-13 19:25:21 +0800

    完成底部导航

---
+++ app/src/main/java/com/test/feiyun/demo/RecordFragment.java
@@ -0,0 +1,19 @@
+package com.test.feiyun.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 飞云 on 2016/8/13.
+ */
+public class RecordFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.record_pg, container, false);
+        return view;
+    }
+
+}
